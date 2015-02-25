fd5d087390ede98712c8b4900f5b7c5fa0b7c9de
Denis Ogun <do1303@my.bristol.ac.uk>
2015-02-25 18:06:22 +0000

    added programmer project

---
+++ Programmer/Context/Context/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Context
+//
+//  Created by Denis Ogun on 25/02/2015.
+//  Copyright (c) 2015 Denis Ogun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
